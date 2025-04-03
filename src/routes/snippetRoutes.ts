b1e2ba258ad735c4f959f9c827ef983feb9feb1b
harshsrivastva97 <harsh.srivastva97@gmail.com>
2025-04-03 15:24:54 +0530

    snippets route fix

--- src/routes/snippetRoutes.ts
+++ src/routes/snippetRoutes.ts
@@ -5,6 +5,7 @@ const router = Router();
 const snippetController = new SnippetController();
 
 router.get("/:user_id", snippetController.getSnippets.bind(snippetController));
+router.get("/", snippetController.getSnippets.bind(snippetController));
 router.get("/snippet/:snippet_id", snippetController.getSnippetById.bind(snippetController));
 
 export default router;
