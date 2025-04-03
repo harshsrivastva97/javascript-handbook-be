09032d8a653da96a14354590aad35c769ecf03af
harshsrivastva97 <harsh.srivastva97@gmail.com>
2025-04-03 13:53:14 +0530

    updated db name

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -5,7 +5,7 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/';
 export const connectMongoDB = async () => {
     try {
         await mongoose.connect(MONGO_URI, {
-            dbName: 'javascript-handbook',
+            dbName: 'javascript-handbook-db',
         });
         console.log('MongoDB connected successfully');
     } catch (error) {
